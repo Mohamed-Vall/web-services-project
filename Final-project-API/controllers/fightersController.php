28e0a48ef8b7d8766c6159db85b9821418818e2a
AtozZone <70588093+AtozZone@users.noreply.github.com>
2022-11-26 15:30:12 -0500

    Update #2
    Update #2

--- Final-project-API/controllers/fightersController.php
+++ Final-project-API/controllers/fightersController.php
@@ -1,6 +1,6 @@
 <?php
 
-class Fighters extends WebServiceInvoker {
+class FightersController extends WebServiceInvoker {
 
     private $request_options = Array(
         'headers' => Array('Accept' => 'application/json')
@@ -15,20 +15,20 @@ class Fighters extends WebServiceInvoker {
      * 
      * @return array containing some information about books. 
      */
-    function getBooksInfo() {
-        $books = Array();
-        $resource_uri = "https://www.anapioficeandfire.com/api/books";
-        $booksData = $this->invoke($resource_uri);
+    function getUpcomingInfo($id) {
+        $events = Array();
+        $resource_uri = "https://api.sportsdata.io/v3/mma/stats/json/Fight/" + $id + "?key=a541ac55a76a48d1add34c4639da10ec";
+        $eventsData = $this->invoke($resource_uri);
 
-        if (!empty($booksData)) {
+        if (!empty($eventsData)) {
             // Parse the fetched list of books.   
-            $booksData = json_decode($booksData, true);
+            $eventsData = json_decode($eventsData, true);
             //var_dump($booksData);exit;
 
             $index = 0;
             // Parse the list of books and retreive some  
             // of the contained information.
-            foreach ($booksData as $key => $book) {
+            foreach ($eventsData as $key => $event) {
                 $books[$index]["name"] = $book["name"];
                 $books[$index]["isbn"] = $book["isbn"];
                 $books[$index]["authors"] = $book["authors"];
@@ -39,5 +39,32 @@ class Fighters extends WebServiceInvoker {
                 $index++;
             }
         }
-        return $books;
+        return $events;
+    }
+
+    function getUpcomingInfo($id) {
+        $events = Array();
+        $resource_uri = "https://api.sportsdata.io/v3/mma/scores/json/Event/" + $id + "?key=a541ac55a76a48d1add34c4639da10ec";
+        $eventsData = $this->invoke($resource_uri);
+
+        if (!empty($eventsData)) {
+            // Parse the fetched list of books.   
+            $eventsData = json_decode($eventsData, true);
+            //var_dump($booksData);exit;
+
+            $index = 0;
+            // Parse the list of books and retreive some  
+            // of the contained information.
+            foreach ($eventsData as $key => $event) {
+                $books[$index]["name"] = $book["name"];
+                $books[$index]["isbn"] = $book["isbn"];
+                $books[$index]["authors"] = $book["authors"];
+                $books[$index]["mediaType"] = $book["mediaType"];
+                $books[$index]["country"] = $book["country"];
+                $books[$index]["released"] = $book["released"];
+                //
+                $index++;
+            }
+        }
+        return $events;
     }
