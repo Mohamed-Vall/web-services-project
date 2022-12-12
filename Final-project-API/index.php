54018be5073d23581fa68488c8e6ceb31f0c6ff8
Mohamed-Vall <mahmoud0uldvall@gmail.com>
2022-12-12 09:04:18 -0500

    Authentification Fix

--- Final-project-API/index.php
+++ Final-project-API/index.php
@@ -14,11 +14,14 @@ require_once './includes/models/FightsModel.php';
 require_once './includes/models/FinalResultsModel.php';
 
 //$client = new GuzzleHttp\Client(['verify' => false ]);
-
+require_once './includes/helpers/JWTManager.php';
 require_once './includes/helpers/WebServiceInvoker.php';
 require_once './controllers/fightersController.php';
 require __DIR__ . '/vendor/autoload.php';
 
+define('APP_BASE_DIR', __DIR__);
+// IMPORTANT: This file must be added to your .ignore file. 
+define('APP_ENV_CONFIG', 'config.env');
 
 //--Step 1) Instantiate App.
 $app = AppFactory::create();
@@ -37,12 +40,31 @@ $errorMiddleware = $app->addErrorMiddleware(true, true, true);
 //$app->setBasePath("/web-services-project/Final-project-API/Final-Project-API"); #change this to subfolder if I need to change e.i. /.../music-api (has to match .htaccess)
 $app->setBasePath("/web-services-project/Final-project-API"); #change this to subfolder if I need to change e.i. /.../music-api (has to match .htaccess)
 $uri_api = "https://api.sportsdata.io/v3/mma/scores/json/Fighters?key=a541ac55a76a48d1add34c4639da10ec";
+$app->add(new Tuupola\Middleware\JwtAuthentication([
+    'secret' => $jwt_secret,
+    'algorithm' => 'HS256',
+    'secure' => false, // only for localhost for prod and test env set true            
+    "path" => $api_base_path, // the base path of the API
+    "attribute" => "decoded_token_data",
+    "ignore" => ["$api_base_path/token", "$api_base_path/account"],
+    "error" => function ($response, $arguments) {
+        $data["status"] = "error";
+        $data["message"] = $arguments["message"];
+        $response->getBody()->write(
+                json_encode($data, JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT)
+        );
+        return $response->withHeader("Content-Type", "application/json;charset=utf-8");
+    }
+]));
 //-- Step 5) Include the files containing the definitions of the callbacks.
 require_once './includes/routes/fighter_routes.php';
 require_once './includes/routes/event_routes.php';
 require_once './includes/routes/finalresult_routes.php';
 require_once './includes/routes/fights_routes.php';
+require_once './includes/routes/token_routes.php';
 
+$app->post("/token", "handleGetToken");
+$app->post("/account", "handleCreateUserAccount");
 //-- Step 6)
 // TODO: And here we define app routes.
 
